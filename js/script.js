932d052fe7046642d615f4ffc0136bcc3ae92648
minasokawus <minasokawus@gmail.com>
2026-07-05 23:17:08 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+﻿const images = [
+    "picture/shouye1.png",
+    "picture/shouye2.png",
+    "picture/shouye3.png"
+];
+
+let index = 0;
+const slide = document.getElementById("slide");
+
+setInterval(() => {
+    index = (index + 1) % images.length;
+    slide.src = images[index];
+}, 3000);
